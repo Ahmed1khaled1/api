6865ed63dfc05b5e3974a2c5a1849d627507051f
Ahmed1khaled1 <ahmedkhaledg49@gmail.com>
2024-11-18 19:44:29 +0300

    varr

--- index.js
+++ index.js
@@ -34,7 +34,9 @@ app.use(cors({
 
 app.options("https://client-rho-dusky-52.vercel.app/", cors()); // Handle preflight requests
 
-mongoose.connect(URI);
+mongoose.connect(
+  "mongodb+srv://ahmedkhaledg49:node-js-123@learn-mongo-db.jvk8d.mongodb.net/mern?retryWrites=true&w=majority&appName=learn-mongo-db"
+);
 
 app.post("/register", async (req, res) => {
   const { username, password } = req.body;
