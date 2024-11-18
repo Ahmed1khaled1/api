694ef227d1a7dbebe8e172ae4e43b2b58089482a
Ahmed1khaled1 <ahmedkhaledg49@gmail.com>
2024-11-18 21:39:52 +0300

    1

--- index.js
+++ index.js
@@ -87,7 +87,11 @@ app.post("/logout", (req, res) => {
 
 app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
   try {
-
+    const { originalname, path } = req.file;
+    const parts = originalname.split(".");
+    const ext = parts[parts.length - 1];
+    const newPath = `${path}.${ext}`;
+    fs.renameSync(path, newPath);
 
     const token = req.cookies.token;
     jwt.verify(token, secret, {}, async (err, info) => {
@@ -99,7 +103,7 @@ app.post("/post", uploadMiddleware.single("file"), async (req, res) => {
         title,
         summary,
         content,
-        
+        cover: newPath,
         author: info.id,
       });
       res.json(postDoc);
