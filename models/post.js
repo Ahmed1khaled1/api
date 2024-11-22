334498f2677dad3d4a7573f2bb5e7140d691f3f0
Ahmed1khaled1 <ahmedkhaledg49@gmail.com>
2024-11-22 20:36:56 +0200

    12

--- models/post.js
+++ models/post.js
@@ -1,14 +1,22 @@
 const mongoose = require('mongoose')
 const { Schema, model } = mongoose;
 
-const postSchema = new Schema({
-  title:String,
-  summary:String,
-  content:String,
-  cover:String,
-  author:{type:String, ref:'User'},
-},{
-  timestamps:true});
+const postSchema = new Schema(
+  {
+    title: String,
+    summary: String,
+    content: String,
+    cover: String,
+    author: { type: String, ref: "User" },
+    authorPic: String,
+    published_date: String,
+    reading_time: String,
+    tags: Array,
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const PostModel = model("Post", postSchema);
 
